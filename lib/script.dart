9d798dd49ae4af6c182b13ce329f708d376b1700
Fathiyyah1 <91325017+Fathiyyah1@users.noreply.github.com>
2023-09-26 22:58:18 +0300

    d.f,mgnijkf

--- lib/script.dart
+++ lib/script.dart
@@ -58,84 +58,96 @@ class _ScriptState extends State<Script> {
         title: const Text('Chemicals'),
         backgroundColor: const Color.fromARGB(255, 68, 107, 103),
       ),
-      body: Column(
+      body: Stack(
         children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: messages.length,
-              itemBuilder: (context, index) {
-                final message = messages[index];
-                return MessageWidget(message: message);
-              },
-            ),
-          ),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                ChoiceChip(
-                  label: Text('Choice 1'),
-                  selected: selectedChoice == 'Choice 1',
-                  onSelected: (selected) {
-                    setState(() {
-                      selectedChoice = selected ? 'Choice 1' : null;
-                    });
-                  },
-                ),
-                const SizedBox(
-                  width: 10,
-                  height: 10,
-                ),
-                ChoiceChip(
-                  label: Text('Choice 2'),
-                  selected: selectedChoice == 'Choice 2',
-                  onSelected: (selected) {
-                    setState(() {
-                      selectedChoice = selected ? 'Choice 2' : null;
-                    });
-                  },
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Row(
+          Column(
             children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: const Color.fromARGB(255, 237, 238, 238),
-                  ),
-                  width: 510,
-                  child: TextFormField(
-                    decoration: const InputDecoration(hintText: "  here..."),
-                  ),
+              Image.asset('asset/images/ant.png'),
+              Image.asset('asset/images/ant.png'),
+            ],
+          ),
+          Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: messages.length,
+                  itemBuilder: (context, index) {
+                    final message = messages[index];
+                    return MessageWidget(message: message);
+                  },
                 ),
               ),
-              IconButton(
-                onPressed: () {
-                  if (selectedChoice != null) {
-                    final isMe = selectedChoice == 'Choice 1';
-                    final message = Message(
-                      text: 'You chose: $selectedChoice',
-                      isMe: isMe,
-                    );
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    ChoiceChip(
+                      label: Text('Threat'),
+                      selected: selectedChoice == 'Threat',
+                      onSelected: (selected) {
+                        setState(() {
+                          selectedChoice = selected ? 'Threat' : null;
+                        });
+                      },
+                    ),
+                    const SizedBox(
+                      width: 10,
+                      height: 10,
+                    ),
+                    ChoiceChip(
+                      label: Text('Danger'),
+                      selected: selectedChoice == 'Danger',
+                      onSelected: (selected) {
+                        setState(() {
+                          selectedChoice = selected ? 'Danger' : null;
+                        });
+                      },
+                    ),
+                    const SizedBox(width: 10, height: 10),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: const Color.fromARGB(255, 237, 238, 238),
+                      ),
+                      width: 510,
+                      child: TextFormField(
+                        decoration:
+                            const InputDecoration(hintText: "  here..."),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      if (selectedChoice != null) {
+                        final isMe = selectedChoice == 'Threat';
+                        final message = Message(
+                          text: 'Call for: $selectedChoice',
+                          isMe: isMe,
+                        );
 
-                    setState(() {
-                      messages.add(message);
-                    });
+                        setState(() {
+                          messages.add(message);
+                        });
 
-                    setState(() {
-                      selectedChoice = null;
-                    });
-                  }
-                },
-                icon: const Icon(Icons.arrow_forward_ios),
-              )
+                        setState(() {
+                          selectedChoice = null;
+                        });
+                      }
+                    },
+                    icon: const Icon(Icons.arrow_forward_ios),
+                  )
+                ],
+              ),
             ],
           ),
         ],
